447b4a9d5339bfdf4f316c5b3d977c05a0d08c9f
Connor Jovan Nel <connor.j.nel00@gmail.com>
2024-02-03 01:33:30 +0000

    py100 basics

---
+++ PY100/projects/python_style.py
@@ -0,0 +1,24 @@
+# Assigning a variable
+forty_two = 42
+v_42 = 42
+
+
+# Defining a function
+def this_is_a_great_method():
+    tell_me_meaning_of_life()
+
+
+# Assigning a constant
+FOUR = "four"
+FIVE = 5
+V_52 = 52
+MEANING_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING = V_52 - 10
+
+
+# Meaning of Life
+def tell_me_meaning_of_life():
+    print(MEANING_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING)
+
+
+# Init Reality
+this_is_a_great_method()
